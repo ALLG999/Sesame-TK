9a8c3f93f5c2df88ecc25dce92e182ac57e25a87
托尼老师五毛钱洗剪吹 <23072523+ALLG999@users.noreply.github.com>
2025-08-08 00:02:52 +0800

    026

--- app/src/main/java/fansirsqi/xposed/sesame/util/DeviceInfo.kt
+++ app/src/main/java/fansirsqi/xposed/sesame/util/DeviceInfo.kt
@@ -6,10 +6,10 @@ import android.content.ClipboardManager
 import android.content.Context
 import android.os.Build
 import android.provider.Settings
-import android.widget.Toast
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.combinedClickable
+import android.widget.Toast
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -24,10 +24,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.PreviewParameterProvider
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import fansirsqi.xposed.sesame.BuildConfig
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 
 class PreviewDeviceInfoProvider : PreviewParameterProvider<Map<String, String>> {
     override val values: Sequence<Map<String, String>> = sequenceOf(
@@ -44,7 +45,6 @@ class PreviewDeviceInfoProvider : PreviewParameterProvider<Map<String, String>> 
         )
     )
 }
-
 
 @Composable
 fun DeviceInfoCard(info: Map<String, String>) {
@@ -85,12 +85,17 @@ fun DeviceInfoCard(info: Map<String, String>) {
                 }
                 Spacer(modifier = Modifier.height(4.dp))
             }
+            // 循环外单独显示红色自定义文字
+            Text(
+                text = "ALLG编译，与原版保持一致。👑",
+                fontSize = 12.sp,
+                color = Color.Red
+            )
         }
     }
 }
 
 object DeviceInfoUtil {
-
     @SuppressLint("HardwareIds")
     fun getDeviceInfo(context: Context): Map<String, String> {
         fun getProp(prop: String): String {
@@ -116,7 +121,6 @@ object DeviceInfoUtil {
             return "${Build.BRAND} ${Build.MODEL}"
         }
 
-
         val androidId = Settings.Secure.getString(context.contentResolver, Settings.Secure.ANDROID_ID)
 
         return mapOf(
@@ -125,7 +129,7 @@ object DeviceInfoUtil {
             "Android Version" to "${Build.VERSION.RELEASE} SDK (${Build.VERSION.SDK_INT})",
             "OS Build" to "${Build.DISPLAY}",
             "Device ID" to androidId,
-            "Module Version" to "${BuildConfig.VERSION}.${BuildConfig.BUILD_TYPE} 📦",
+            "Module Version" to "${BuildConfig.VERSION}-${BuildConfig.BUILD_TAG}.${BuildConfig.BUILD_TYPE} 📦",
             "Module Build" to "${BuildConfig.BUILD_DATE} ${BuildConfig.BUILD_TIME} ⏰"
         )
     }
