11b8b5c31efd139f0fea15cf6d3a16f0575d33a0
托尼老师五毛钱洗剪吹 <23072523+ALLG999@users.noreply.github.com>
2025-09-17 21:13:16 +0800

    放出新村和神奇物种
    神奇物质容易报错❌

--- app/src/main/java/fansirsqi/xposed/sesame/model/ModelOrder.java
+++ app/src/main/java/fansirsqi/xposed/sesame/model/ModelOrder.java
@@ -25,13 +25,13 @@ public class ModelOrder {
             AntFarm.class,//庄园
             AntOrchard.class,//农场
             AntOcean.class,//海洋
-//          AntDodo.class,//神奇物种
+            AntDodo.class,//神奇物种
 //          AncientTree.class,//古树
 //          AntCooperate.class,//合种
 //          Reserve.class,//保护地
             AntSports.class,//运动
             AntMember.class,//会员
-//          AntStall.class,//蚂蚁新村
+            AntStall.class,//蚂蚁新村
 //          GreenFinance.class,//绿色经营
 //          AntBookRead.class,//读书
 //          ConsumeGold.class,//消费金
